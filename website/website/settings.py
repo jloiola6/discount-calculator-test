59427bb5e6ed1107ac9544603b68680ac5dbb724
João Teixeira <jloiola6@outlook.com>
2023-04-12 11:49:18 -0300

    Retirando Secret_key de um arquivo e deixando direto no settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-import environ
-
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-zc248bcznv42azj#3=w@&ow&a%&0&f_jf+aul3_j1d%265k9yc'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
